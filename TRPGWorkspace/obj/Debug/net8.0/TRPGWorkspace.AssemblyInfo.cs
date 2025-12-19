9dde7ff6ccc0d000a9cc342fec0d814cb8869c64
BIGSEAFISH <3047813499@qq.com>
2025-12-20 01:13:05 +0800

    优化移动端UI

--- TRPGWorkspace/obj/Debug/net8.0/TRPGWorkspace.AssemblyInfo.cs
+++ TRPGWorkspace/obj/Debug/net8.0/TRPGWorkspace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TRPGWorkspace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531f7238027cb456b70bf216ffcfecbd72634bb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391f13473777b9f3ddde94f403b755969a66c3ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("TRPGWorkspace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TRPGWorkspace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
